9f4979cf6f1c75dde9d0fcb6d2d18bb701f4b924
xiongshaowen <65179334@qq.com>
2023-04-13 19:17:00 +0800

    Initial commit

---
+++ src/main/java/com/qcl/global/GlobalConst.java
@@ -0,0 +1,15 @@
+package com.qcl.global;
+
+/**
+ * 作者：编程小石头
+ * 一些全局的常量
+ */
+public interface GlobalConst {
+    String COOKIE_TOKEN = "bianchengxiaoshitou";//用来管理Cookie的key
+    /*
+    * 小程序相关
+    * */
+    String APPID="wx6be7eb3f3a4ecad3";//小程序的appid，记得换成你自己的  65179334@qq.com
+    String APPSECRET = "f695c65900f5b85a484aaa8ac1329dfe";//小程序的appsecret，记得换成你自己的
+
+}
